289188cef21e19acf960961908157a993df455ba
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-07 03:43:05 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,12 @@
 class Project
-  attr_accessor :name
+  attr_accessor :name, :backer
 
   def initialize(name)
     @name = name
   end
+
+  def add_backer(name)
+    @backer = name
+  end
+    
 end
